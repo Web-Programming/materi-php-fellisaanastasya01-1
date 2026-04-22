e12e7d47f0550c8a5837c8b60f43807aad7790fa
fellisaanastasya01 <anastasyafellisa@gmail.com>
2026-04-22 14:37:30 +0700

    add test_method dan web.php

--- fellisa-app/routes/web.php
+++ fellisa-app/routes/web.php
@@ -1,7 +1,23 @@
-<?php
+//Router dengan metode POST
+Route::post('/simpan', function(){
+    echo "Data berhasil disimpan";
+});
 
-use Illuminate\Support\Facades\Route;
+//Router dengan metode PUT
+Route::put('/update/{id}', function($id){
+    echo "Data berhasil diperbarui dengan ID: " . $id;
+});
 
-Route::get('/', function () {
-    return view('welcome');
+//Router dengan metode PATCH
+Route::patch('/update2/{id}', function($id){
+    echo "Data berhasil diperbarui dengan ID: " . $id;
+});
+
+//Router dengan metode DELETE
+Route::delete('/hapus/{id}', function($id){
+    echo "Data berhasil dihapus dengan ID: " . $id;
+});
+//Route untuk menampilkan halaman test_method
+Route::get('/test-method', function(){
+    return view('test_method');
 });
